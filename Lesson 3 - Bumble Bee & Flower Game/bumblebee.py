4de5dd65c4e31e5f1191495759de2cf5347050cc
Sarang Menon <sarangmenon2011@gmail.com>
2024-09-07 10:56:20 +0530

    Add files via upload

---
+++ Lesson 3 - Bumble Bee & Flower Game/bumblebee.py
@@ -0,0 +1,41 @@
+import pgzrun
+import random
+
+WIDTH = 600
+HEIGHT = 500
+
+bee = Actor("bee")
+bee.pos = 100, 100
+
+flower = Actor("flower")
+flower.pos = 200, 200
+
+score = 0
+
+def draw():
+    screen.blit("background", (0, 0))
+    bee.draw()
+    flower.draw()
+    screen.draw.text("Score: "+str(score), color = "blue", topleft = (10, 20))
+
+def placeflower():
+    flower.x = random.randint(70, (WIDTH-70))
+    flower.y = random.randint(70, (HEIGHT-70))
+
+def update():
+    global score
+    if keyboard.left:
+        bee.x = bee.x-2
+    if keyboard.right:
+        bee.x = bee.x+2
+    if keyboard.up:
+        bee.y = bee.y-2
+    if keyboard.down:
+        bee.y = bee.y+2
+    flowercollected = bee.colliderect(flower)
+    if flowercollected:
+        score = score+10
+        placeflower()
+    
+
+pgzrun.go()
